84cff4f05b2c3dc0d73a1d0e6b5ffcef6e78c448
Pat Viafore <patviafore@gmail.com>
2023-12-02 22:20:50 -0600

    Finally get 16 and 17

--- challenge16.py
+++ challenge16.py
@@ -1,9 +1,9 @@
+from functools import cache
 from itertools import chain, cycle, starmap
 from operator import mul
 from typing import Iterator, List
 
 from common.input_handling import get_input
-
 def to_signal(text: str) -> List[int]:
     return [int(s) for s in text]
 
@@ -16,21 +16,44 @@ def get_fft_digits(signal: List[int], number_of_phases: int) -> str:
     return "".join(str(i) for i in signal[:8])
 
 def fft(signal: List[int]) -> List[int]:
-    return [calculate_fft_digit(index, signal) for index in range(1, len(signal) + 1)]
+    return [calculate_fft_digit(index, signal) for index in range(len(signal))]
 
 def calculate_fft_digit(index: int, signal: List[int]) -> int:
-    pattern = calculate_pattern(index)
+    pattern = calculate_cycled_pattern(index)
     return abs(sum(starmap(mul, zip(pattern, signal)))) % 10
 
-def calculate_pattern(index: int) -> Iterator[int]:
-    base_pattern = [0, 1, 0, -1]
-    pattern = cycle(chain.from_iterable([n] * index for n in base_pattern))
-    # exhaust first element
+
+def calculate_cycled_pattern(index: int) -> Iterator[int]:
+    pattern = cycle(chain.from_iterable(calculate_pattern(index+1)))
     next(pattern)
     return pattern
 
+@cache
+def calculate_pattern(index: int) -> list[list[int]]:
+    base_pattern = [0, 1, 0, -1]
+    full_pattern = [[n] * index for n in base_pattern]
+    return full_pattern
+
+
+def get_digit_at(position: int) -> list[int]:
+    pattern_index = position - ( position // len(SIGNAL) * len(SIGNAL)) - 1
+    return SIGNAL[pattern_index]
+
+def get_fft_extended(phases: int) -> str:
+    offset = int("".join(str(i) for i in SIGNAL[:7]))+1
+    print(offset)
+    numbers = [get_digit_at(n) for n in range(offset, len(SIGNAL)*10000)]
+    for phase in range(phases):
+        print(f"PHASE {phase}")
+        total_numbers = sum(numbers)
+        new_numbers = []
+        for index in range(len(numbers)):
+            new_numbers.append(total_numbers % 10)
+            total_numbers -= numbers[index]
+        numbers = new_numbers
+    return "".join(str(n) for n in numbers[:8])
 
 SIGNAL = get_signal()
 if __name__ == "__main__":
     print(get_fft_digits(SIGNAL, 100))
-    print(get_fft_digits(SIGNAL*10000, 100))
+    print(get_fft_extended(100))
--- challenge17.py
+++ challenge17.py
@@ -1,5 +1,7 @@
-from typing import Generator, List
+import itertools
 
+from typing import List
+from enum import Enum
 from common.cartesian import get_orthogonal_points, Point, TextGrid
 from common.input_handling import get_input
 from common.intcode_computer import Computer, ParameterMode
@@ -11,22 +13,63 @@ def is_surrounded_by_scaffolds(scaffold: Point, text_grid: TextGrid) -> bool:
     adjacent_points = get_orthogonal_points(scaffold)
     return all(text_grid.get(p) == '#' for p in adjacent_points)
 
-def to_left(symbol: str) -> str:
+class Orientation(Enum):
+    LEFT=1,
+    RIGHT=2,
+    UP=3,
+    DOWN=4
+
+    def get_next_point(self, point: Point) -> Point:
+        match self:
+            case Orientation.UP: return point.to_above()
+            case Orientation.DOWN: return point.to_below()
+            case Orientation.LEFT: return point.to_left()
+            case Orientation.RIGHT: return point.to_right()
+        assert False, "Out of orientations"
+    
+    def turn_left(self) -> 'Orientation':
+        match self:
+            case Orientation.UP: return Orientation.LEFT
+            case Orientation.DOWN: return Orientation.RIGHT
+            case Orientation.LEFT: return Orientation.DOWN
+            case Orientation.RIGHT: return Orientation.UP
+        assert False, "Out of orientations"
+    
+    def turn_right(self) -> 'Orientation':
+        match self:
+            case Orientation.UP: return Orientation.RIGHT
+            case Orientation.DOWN: return Orientation.LEFT
+            case Orientation.LEFT: return Orientation.UP
+            case Orientation.RIGHT: return Orientation.DOWN
+        assert False, "Out of orientations"
+    
+    def opposite(self) -> 'Orientation':
+        match self:
+            case Orientation.UP: return Orientation.DOWN
+            case Orientation.DOWN: return Orientation.UP
+            case Orientation.LEFT: return Orientation.RIGHT
+            case Orientation.RIGHT: return Orientation.LEFT
+        assert False, "Out of orientations"
+
+    def __str__(self) -> str:
+        match self:
+            case Orientation.UP: return "U"
+            case Orientation.DOWN: return "D"
+            case Orientation.LEFT: return "L"
+            case Orientation.RIGHT: return "R"
+        assert False, "Out of orientations"
+
+
+    
+def get_orientation(symbol: str) -> Orientation:
     return {
-        '<': 'v',
-        'v': '>',
-        '>': '^',
-        '^': '<'
+        '<': Orientation.LEFT,
+        '>': Orientation.RIGHT,
+        '^': Orientation.UP,
+        'v': Orientation.DOWN,
     }[symbol]
 
-def to_right(symbol: str) -> str:
-    return {
-        '<': '^',
-        'v': '<',
-        '>': 'v',
-        '^': '>'
-    }[symbol]
-
+Moves = list[int | Orientation]
 class Scaffolding:
 
     def __init__(self):
@@ -42,18 +85,93 @@ class Scaffolding:
         text_grid = TextGrid(self.points.split('\n'))
         scaffolds = text_grid.get_all_points_matching_symbol('#')
         return [s for s in scaffolds if is_surrounded_by_scaffolds(s, text_grid)]
+    
 
-    def get_possible_routes(self) -> Generator[str, None, None]:
-        # text_grid = TextGrid(self.points.split('\n'))
-        # routes = []
-        while self.points:
-            #current_position = text_grid.get_all_points_matching_function(lambda c: c in '<<>^v')
-            # left = to_left(text_grid[current_position[0]])
-            # right = to_right(text_grid[current_position[0]])
-            break
-        # to-do figure this out later
-        yield 'abc'
+    def get_next_orientation(self, text_grid: TextGrid, next_point: Point, next_orientation: Orientation) -> tuple[Orientation, Orientation] | None:
+        if text_grid.get(next_orientation.turn_left().get_next_point(next_point)) == '#':
+            return (Orientation.LEFT, next_orientation.turn_left())
+        elif text_grid.get(next_orientation.turn_right().get_next_point(next_point)) == '#':
+            return (Orientation.RIGHT, next_orientation.turn_right())
+        return None
 
+
+    def get_route(self) -> Moves:
+        text_grid = TextGrid(self.points.split('\n'))
+        current_position = text_grid.get_all_points_matching_function(lambda c: c in '<<>^v')[0]
+        current_orientation = get_orientation(text_grid.get(current_position))
+        moves : Moves = []
+        if text_grid.get(current_orientation.get_next_point(current_position )) == '.':
+            turn, current_orientation = self.get_next_orientation(text_grid, current_position, current_orientation)
+            moves.append(turn)
+
+        last_move = 0
+        next_point = current_position
+        next_orientation = current_orientation
+        while True:
+            new_point = next_orientation.get_next_point(next_point)
+            if text_grid.get(new_point) == '#':
+                last_move += 1 
+                next_point = new_point
+            else:
+                moves.append(last_move)
+                last_move = 0
+                next_move = self.get_next_orientation(text_grid, next_point, next_orientation)
+                if not next_move:
+                    break  # we are at the end
+                turn, next_orientation = next_move
+                moves.append(turn.opposite())  # we do the opposite because our coorindate system is not upside down
+        return moves
+
+    
+    def get_programs(self):
+        route = self.get_route()
+        max_index = 100 
+        for a_len, b_len, c_len in itertools.product(range(1,11), range(1,11), range(1,11)):
+            candidates = [([], [], [], [], list(route))]
+            while candidates:
+                path,a,b,c,remaining_route = candidates.pop()
+                if len(",".join(path)) > 20 or len(",".join(str(x) for x in a)) > 20 or len(",".join(str(x) for x in b)) > 20 or len(",".join(str(x) for x in c)) > 20:
+                    continue
+                if not remaining_route:
+                    return path, a, b, c
+                if a and remaining_route[:len(a)] == a:
+                    candidates.append((path + ['A'], a, b, c, remaining_route[len(a):]))
+                if b and remaining_route[:len(b)] == b:
+                    candidates.append((path + ['B'], a, b, c, remaining_route[len(b):]))
+                if c and remaining_route[:len(c)] == c:
+                    candidates.append((path + ['C'], a, b, c, remaining_route[len(c):]))
+                if not a:
+                    candidates.append((path+['A'], remaining_route[:a_len], b, c, remaining_route[a_len:])) # assign a to a certain length
+                    last_value = remaining_route[a_len - 1]
+                    if type(last_value) == int:
+                        for val in range(1,last_value):
+                            new_remaining_route = remaining_route[:a_len - 1] + [val, last_value - val] + remaining_route[a_len + 2:]
+                            candidates.append((path+['A'], new_remaining_route[:a_len], b, c, new_remaining_route[a_len:])) # assign a to a certain length
+                elif not b:
+                    candidates.append((path+['B'], a, remaining_route[:b_len], c, remaining_route[b_len:])) # assign b to a certain length
+                    last_value = remaining_route[b_len - 1]
+                    if type(last_value) == int:
+                        for val in range(1,last_value):
+                            new_remaining_route = remaining_route[:b_len - 1] + [val, last_value - val] + remaining_route[b_len + 2:]
+                            candidates.append((path+['B'], a, new_remaining_route[:b_len], c, new_remaining_route[b_len:])) # assign b to a certain length
+                elif not c:
+                    candidates.append((path+['C'], a, b, remaining_route[:c_len], remaining_route[c_len:])) # assign c to a certain length
+                    last_value = remaining_route[c_len - 1]
+                    if type(last_value) == int:
+                        for val in range(1,last_value):
+                            new_remaining_route = remaining_route[:c_len - 1] + [val, last_value - val] + remaining_route[c_len + 2:]
+                            candidates.append((path+['C'], a, b, new_remaining_route[:c_len], new_remaining_route[c_len:])) # assign c to a certain length
+        assert False, "Should not reach here"
+
+def get_alignment(intersection: Point) -> int:
+    return intersection.x * intersection.y
+
+def run_program():
+    computer = Computer(load_instructions())
+    scaffolding = Scaffolding()
+    computer.output_function = scaffolding.plot_point
+    computer.run_loop()
+    return sum(get_alignment(i) for i in scaffolding.find_intersections())
 
 
 def get_alignment(intersection: Point) -> int:
@@ -66,28 +184,38 @@ def run_program():
     computer.run_loop()
     return sum(get_alignment(i) for i in scaffolding.find_intersections())
 
-def get_functions(_possible_route: str) -> List[str]:
-    return []
 
 class VacuumRobot:
     def __init__(self, scaffolding: Scaffolding):
         self.scaffolding = scaffolding
 
+        self.programs = self.scaffolding.get_programs()
         self.computer = Computer(load_instructions())
         self.computer.write(0, ParameterMode.POSITIONAL, 2)
-        self.computer.input_function = self.get_movement_routines
-        for possible_route in scaffolding.get_possible_routes():
-            functions = get_functions(possible_route)
-            if len(functions) == 4:
-                self.functions = functions
-                break
-        self.functions.append('n')
+        self.computer.output_function = self.read_dust
+        self.computer.input_function = self.get_programs
+        self.dust = 0
+        self.line = None
+        self.sent_visual = False
+
+    def get_programs(self):
+        if self.programs or self.line:
+            if not self.line:
+                self.line = ",".join(str(x) for x in self.programs[0]) + "\n"
+                self.programs = self.programs[1:]
+            c = self.line[0]
+            self.line = self.line[1:]
+            return ord(c)
+        if not self.sent_visual:
+            self.sent_visual = True
+            return ord('n')
+        return ord("\n")
 
     def run(self):
         self.computer.run_loop()
 
-    def get_movement_routines(self) -> int:
-        return ord(self.functions.pop(0)) if self.functions else ord("\n")
+    def read_dust(self, val):
+        self.dust = val
 
 
 def get_space_dust() -> int:
@@ -98,7 +226,7 @@ def get_space_dust() -> int:
 
     robot = VacuumRobot(scaffolding)
     robot.run()
-    return 0
+    return robot.dust 
 
 if __name__ == "__main__":
     print(run_program())
